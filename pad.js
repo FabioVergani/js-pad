9f966fb9ab9a995e0fc36d5b29f61ede8b66dae1
FabioVergani <fabio.vergani.1975@gmail.com>
2014-08-24 14:47:16 +0200

    Update pad.js

--- pad.js
+++ pad.js
@@ -3,3 +3,8 @@ function pad(s,z){
 	if(n>0){z=f(z);l=Math.max(n-r[l],0);if(l>0){r=z.substr(0,l)+s}};
 	return r
 }
+
+
+console.log(pad(123456,'abcdefghilm'));
+console.log(pad(10,'000'));
+console.log(pad(1000,'00'));
